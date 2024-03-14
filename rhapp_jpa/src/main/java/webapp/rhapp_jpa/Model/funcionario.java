f84d3942821839284cf6366cc4b2894e011b28c6
Hunter7210 <heitorrmsilva@gmail.com>
2024-03-14 13:56:37 -0300

    Iniciando no JPA

---
+++ rhapp_jpa/src/main/java/webapp/rhapp_jpa/Model/funcionario.java
@@ -0,0 +1,31 @@
+package webapp.rhapp_jpa.Model;
+
+public class funcionario {
+    private long id;
+    private String nome;
+    private String email;
+
+    //Criação dos Getters and Setters
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+
+
+    
+}
